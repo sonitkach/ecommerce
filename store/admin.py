dac164096348e795272a78380584c9bef0bd8b45
ASUS <sonitkach@gmail.com>
2020-12-07 18:19:37 +0300

    registered models in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Order, OrderedItem, Address, Product
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderedItem)
+admin.site.register(Address)
+admin.site.register(Product)
