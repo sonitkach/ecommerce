51cd696a53c58f08029af0ebe3f934162c35b71d
ASUS <sonitkach@gmail.com>
2020-12-12 13:02:12 +0300

    create query data for cart (views)

--- store/views.py
+++ store/views.py
@@ -11,5 +11,12 @@ def checkout(request):
     return render(request, 'store/checkout.html', context)
 
 def cart(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items=[]
+
+    context ={'items':items}
     return render(request, 'store/cart.html', context)
