0be2674b36c9a352946a1bb11a7dde97ba14e729
ASUS <sonitkach@gmail.com>
2020-12-13 03:32:40 +0300

    calculation and rendering totals in cart

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Order, OrderedItem, Address, Product
+from .models import Customer, Order, OrderItem, Address, Product
 
 admin.site.register(Customer)
 admin.site.register(Order)
-admin.site.register(OrderedItem)
+admin.site.register(OrderItem)
 admin.site.register(Address)
 admin.site.register(Product)
